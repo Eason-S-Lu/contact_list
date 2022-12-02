700c8810a6baa21f3bae0f153f53192e8c1229e1
Eason Lu <github@easonlu.ml>
2022-12-02 23:14:29 +0100

    int main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <string>
+#include <locale>
+#include <algorithm>
+#include <stdexcept>
+using namespace std;
+int main (){
+
+    return 0;
+}
